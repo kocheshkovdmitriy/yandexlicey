19b6c2bdbf9bf4658e040ba0f9c4b3d3bcc0fd28
Dmitrii <kocheshkovds@gmail.com>
2024-01-11 19:05:54 +0300

    решение задачи "размер всех файлом"

--- WEB1_os_json_zip/lesson.py
+++ WEB1_os_json_zip/lesson.py
@@ -1,2 +1,15 @@
 # Материалы урока
 
+import os
+from task_HRF import human_read_format
+
+def get_files_sizes():
+    for el in os.listdir('data'):
+        path = os.path.join('data', el)
+        if os.path.isfile(path):
+            size = os.path.getsize(path)
+            print(path, human_read_format(size))
+            size = os.stat(path)
+            print(path, human_read_format(size.st_size))
+
+get_files_sizes()
--- WEB1_os_json_zip/task_size_all_files.py
+++ WEB1_os_json_zip/task_size_all_files.py
@@ -1,1 +1,13 @@
 # решение второй классной задачи
+import os
+from task_HRF import human_read_format
+
+def get_files_sizes():
+    for el in os.listdir('data'):
+        path = os.path.join('data', el)
+        if os.path.isfile(path):
+            size = os.path.getsize(path)
+            print(path, human_read_format(size))
+
+
+get_files_sizes()
