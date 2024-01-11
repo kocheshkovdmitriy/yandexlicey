fccdfd3eb044deb24bcc3829d677750f6ef972a4
Dmitrii <kocheshkovds@gmail.com>
2024-01-11 18:28:23 +0300

    решение задачи "ЧЧФ"

--- WEB1_os_json_zip/task_HRF.py
+++ WEB1_os_json_zip/task_HRF.py
@@ -1,1 +1,12 @@
 # Решение первой класной задачи
+
+def human_read_format(size):
+    data = ['', 'К', 'М', 'Г', 'Т']
+    for i in range(5):
+        if size // 1024 ** i < 1024:
+            return f'{round(size / 1024 ** i)}{data[i]}Б'
+
+
+if __name__ == '__main__':
+    for size in [1023, 1024, 2040, 15000, 10 ** 7, 10 ** 10]:
+        print(f'{size}: {human_read_format(size)}')
